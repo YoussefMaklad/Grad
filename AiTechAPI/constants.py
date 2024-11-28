397a90021f65fd8e41e12eeaa92cc6ebc64666ea
YousefMakalad <youssefmfmakled@gmail.com>
2024-11-28 17:42:59 +0000

    add api to aitech

---
+++ AiTechAPI/constants.py
@@ -0,0 +1,18 @@
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, just "
+    "reformulate it if needed and otherwise return it as is."
+)
+
+# Answer question prompt
+# This system prompt helps the AI understand that it should provide concise answers
+# based on the retrieved context and indicates what to do if the answer is unknown
+qa_system_prompt = (
+    "You are an assistant for question-answering tasks. Use "
+    "the following pieces of retrieved context to answer the "
+    "question. If you don't know the answer, just say that you "
+    "don't know. \n\n"
+    "Context: {context}"
+)
---
+++ AiTechAPI/models.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+class MessageItem(BaseModel):
+    role: str
+    content: str
+
+class ChoiceItem(BaseModel):
+    index: int
+    message: MessageItem
+    finish_reason: str
+
+class RagModelRequestModel(BaseModel):
+    model: str
+    messages: list[MessageItem]
+    
+class RagModelResponseModel(BaseModel):
+    lm_response: str
+
+class FuzzerRequestModel(BaseModel):
+    model: str
+    messages: list[MessageItem]
+    
+class FuzzerResponseModel(BaseModel):
+    id: str
+    object: str
+    created: int
+    model: str
+    choices: list[ChoiceItem]
+    usage: dict        
