adfe96e46f8c16d72fbb8e1f99d4f7ca3a00fc6e
YoussefMaklad <145257225+YoussefMaklad@users.noreply.github.com>
2024-12-09 14:25:43 +0200

    Update deps.sh

--- deps.sh
+++ deps.sh
@@ -2,11 +2,8 @@
 sudo apt-get update
 sudo apt-get install -y docker python3 python3-pip
 pip3 install matplotlib pandas
-pip3 install python-dotenv langchain langchain_openai langchain_chroma openai chromadb tiktoken tavily-python
-pip3 install fastapi pydantic uvicorn
-pip3 install -U langchain-community
-pip3 install -U langchain_huggingface
-pip3 install -qU langchain-google-genai
-pip3 install -U bitsandbytes
-pip3 install transformers accelerate
-pip3 install --upgrade transformers accelerate
+
+# sudo cd python-api
+# sudo python -m venv venv
+# source venv/bin/activate
+# pip3 install -r requirements.txt
